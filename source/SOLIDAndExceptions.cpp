defd82c692c39afc942f6439579763db28cbdbe5
Victor Leskin <victorleskin16@gmail.com>
2025-04-26 15:35:54 +0300

    Make common working state

--- source/SOLIDAndExceptions.cpp
+++ source/SOLIDAndExceptions.cpp
@@ -1,5 +1,43 @@
 ///************************* ITELMA SP ****************************************
 
 #include "SOLIDAndExceptions.hpp"
+#include <cassert>
 
+void SOLIDAndExceptions::run()
+{
+    bool stop = false;
+    cExceptionsHandler handler(commands);
 
+    while (!stop) 
+    {
+        iCommand* cmd = commands.front();
+        commands.pop_front();
+
+        try 
+        {
+            cmd->Execute();
+        }
+        catch (std::exception &e) 
+        {
+            handler.Handle(*cmd, e).Execute();
+        }
+    }
+
+}
+
+class cEndOfUniverse : public iCommand
+{
+    virtual void Execute() override;
+    virtual const char* Type() { return "End of Universe"; }
+};
+
+iCommand& cExceptionsHandler::Handle(iCommand& command, std::exception& e)
+{
+    static cEndOfUniverse endOfEverything;
+    return endOfEverything;
+}
+
+void cEndOfUniverse::Execute()
+{
+    assert(false);
+}
--- source/SOLIDAndExceptions.hpp
+++ source/SOLIDAndExceptions.hpp
@@ -2,8 +2,35 @@
 #ifndef SOLIDANDEXCEPTIONS_HPP
 #define SOLIDANDEXCEPTIONS_HPP
 
+#include <deque>
+#include <string>
+
+class iCommand // interface class of command
+{
+public:
+	virtual void Execute() = 0;
+	virtual const char* Type() = 0;
+};
+
+class cExceptionsHandler
+{
+public:
+	cExceptionsHandler(std::deque<iCommand*>& c) : commandsDeque(&c) {}
+	
+	iCommand& Handle(iCommand& command, std::exception& e);
+
+	std::deque<iCommand*> *commandsDeque;
+};
+
+
 class SOLIDAndExceptions
 {
+public:
+    void run();
+
+protected:
+	std::deque<iCommand*> commands;
+
 };
 
 #endif //#ifndef SOLIDANDEXCEPTIONS_HPP
